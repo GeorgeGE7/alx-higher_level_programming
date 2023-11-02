3c8a5b488b928ffba7c3c377aa97dcfcd3ef5d4a
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-11-02 13:32:55 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,22 @@
 from calculator_1 import add, sub, mul, div
 
 
-def arg_calc(argv):
+def summingA(argv):
     n = len(argv) - 1
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
-    op = argv[2]
+    tshg = argv[2]
     b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    if tshg == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, tshg, b, add(a, b)))
+    elif tshg == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, tshg, b, sub(a, b)))
+    elif tshg == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, tshg, b, mul(a, b)))
+    elif tshg == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, tshg, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
@@ -25,4 +25,4 @@ def arg_calc(argv):
 
 if __name__ == "__main__":
     import sys
-    arg_calc(sys.argv)
+    summingA(sys.argv)
