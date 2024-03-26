b299cb362e5f736dbb0cd2b408c27b69964a97c5
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-03-26 20:00:38 +0200

    test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[1], 'utf8', function (err, fileContent) {
+fs.readFile(process.argv[2], 'utf8', function (err, fileContent) {
   console.log(fileContent || err);
 });
